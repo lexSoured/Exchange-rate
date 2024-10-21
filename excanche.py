d487195335561cb1700dde8a5389c3bd50876837
lexSoured <135127349+lexSoured@users.noreply.github.com>
2024-10-21 10:25:35 +0300

    Revert "Entry изменен на combobox"

--- excanche.py
+++ excanche.py
@@ -5,7 +5,7 @@ from tkinter import messagebox, ttk
 
 
 def exchange():
-    code = combobox.get()
+    code = ent.get()
 
     if code:
         try:
@@ -14,26 +14,21 @@ def exchange():
             data = response.json()
             if code in data['rates']:
                 exchange_rate = data['rates'][code]
-                messagebox.showinfo('Exchange rate', f'Rate: {exchange_rate:.3f} -{code}')
+                messagebox.showinfo('Exchange rate', f'Rate: {exchange_rate} -{code}')
             else:
                  messagebox.showerror('Error', f'Currency with {code} not found' )    
         except Exception as e:
                 messagebox.showerror('Error', f'Error - {e}')
-    else:
-         messagebox.showwarning('Attention', 'Entry is empty')
 
 
 root = tk.Tk()
 root.title('Currency exchange rates')
 root.geometry('500x500')
 
-tk.Label(text='Select currency code').pack(padx=10, pady=10)
-currency_dict = ['RUB', 'EUR', 'USD', 'GBP', 'CNY', 'AZN', "BYN", "JPY", "TJS" , "TRY",]
-combobox = ttk.Combobox(values=currency_dict)
-combobox.pack()
+tk.Label(text='Enter currency code').pack(padx=10, pady=10)
 
-# ent = ttk.Entry(root, width=30, font=("Arial", 12))
-# ent.pack()
-# ent.config(background="white", foreground="black", state="normal", style="TEntry", cursor="arrow", takefocus=1)
+ent = ttk.Entry(root, width=30, font=("Arial", 12))
+ent.pack()
+ent.config(background="white", foreground="black", state="normal", style="TEntry", cursor="arrow", takefocus=1)
 tk.Button(text='Get exchange rate', justify='center' , command=exchange).pack(padx=10, pady=10)
 root.mainloop()
