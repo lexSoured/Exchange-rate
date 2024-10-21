44185a8c5a470559d8e40c426824113e15acdd27
lexsoured <lexsoured@gmail.com>
2024-10-21 15:19:58 +0300

    Добавлено значение целевой валюты по умолчанию с обновлением метки при загрузке программы. Добавлен второй комбобокс с выбором базовой валюты.

--- excanche.py
+++ excanche.py
@@ -13,30 +13,32 @@ def load_currencies():
     return currencies
 
 
-def update_currency_label(event):
+def update_label(combobox, label):
     """Обновляет метку с названием выбранной валюты."""
-    selected_code = event.widget.get()
+    selected_code = combobox.get()
     currency_name = currencies[selected_code]
-    currency_label.config(text=currency_name)
+    label.config(text=currency_name)
 
 
 def get_exchange_rate():
     """Получает курс обмена валюты."""
-    selected_code = combobox.get()
+    target_code = target_currency_combobox.get()
+    base_code = base_currency_combobox.get()
 
-    if selected_code:
+    if target_code and base_code:
         try:
-            response = requests.get('https://open.er-api.com/v6/latest/RUB')
+            response = requests.get(f'https://open.er-api.com/v6/latest/{base_code}')
             response.raise_for_status()
             data = response.json()
-            if selected_code in data['rates']:
-                exchange_rate = data['rates'][selected_code]
-                currency_name = currencies[selected_code]
+            if target_code in data['rates']:
+                exchange_rate = data['rates'][target_code]
+                target_name = currencies[target_code]
+                base_name = currencies[base_code]
                 messagebox.showinfo('Курс обмена', f'Курс: {
-                                    exchange_rate:.3f} - {currency_name} за 1 РУБ')
+                                    exchange_rate:.3f} - {target_name} за 1 {base_name}')
             else:
                 messagebox.showerror('Ошибка', f'Валюта с кодом {
-                                     selected_code} не найдена')
+                                     target_code} не найдена')
         except Exception as e:
             messagebox.showerror('Ошибка', f'Ошибка - {e}')
     else:
@@ -49,23 +51,35 @@ root.geometry('500x500')
 
 # Загрузка данных о валютах
 currencies = load_currencies()
+# Метка и выпадающий список для первой базовой валюты
+base_currency_label = ttk.Label(root, text='Выберите базовую валюту:')
+base_currency_label.pack(pady=[10, 10])  # Размещаем метку в окне
 
-# Создание метки для выбора валюты
-tk.Label(text='Выберите код валюты').pack(padx=10, pady=10)
+base_currency_combobox = ttk.Combobox(width=30, height=30, values=list(currencies.keys()))
+base_currency_combobox.pack(pady=[10, 10])  # Размещаем выпадающий список в окне
+base_currency_combobox.bind('<<ComboboxSelected>>', lambda event: update_label(base_currency_combobox, base_label))
+base_label = tk.Label()
+base_label.pack(padx=10, pady=10)
+
+# Метка и выпадающий список для целевой валюты
+target_currency_label = ttk.Label(root, text='Выберите код целевой валюты:')
+target_currency_label.pack(pady=[10, 10])  # Размещаем метку в окне
 
 # Создание выпадающего списка с кодами валют
-combobox = ttk.Combobox(width=30, height=30, values=list(currencies.keys()))
-combobox.pack()
-combobox.current(0)
-combobox.bind('<<ComboboxSelected>>', update_currency_label)
+target_currency_combobox = ttk.Combobox(width=30, height=30, values=list(currencies.keys()))
+target_currency_combobox.pack()
+target_currency_combobox.current(0)
+target_currency_combobox.bind('<<ComboboxSelected>>', lambda event: update_label(target_currency_combobox, target_label))
 
 # Метка для отображения названия выбранной валюты
-currency_label = tk.Label(text='Российский рубль')
-currency_label.pack(padx=10, pady=10)
+target_label = tk.Label()
+target_label.pack(padx=10, pady=10)
 
 # Кнопка для получения курса обмена
 tk.Button(text='Получить курс обмена', justify='center',
           command=get_exchange_rate).pack(padx=10, pady=10)
+
+update_label(target_currency_combobox, target_label)
 
 # Запуск основного цикла приложения
 root.mainloop()
